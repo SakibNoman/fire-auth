b7f9861fcb229db80ca10ada097a48d3a06a875f
Sakib Noman <skbnmn@gmail.com>
2021-03-13 22:46:12 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCN5wiG8UThTzICYThi8EvI2Ssj9vRKcUU",
+    authDomain: "ema-john-fire.firebaseapp.com",
+    projectId: "ema-john-fire",
+    storageBucket: "ema-john-fire.appspot.com",
+    messagingSenderId: "458296983718",
+    appId: "1:458296983718:web:9529b80a31a8c33b32b467"
+};
+
+export default firebaseConfig;
