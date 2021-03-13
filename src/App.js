ee6618040c0914273d1f0f06e132ea6c781f7acd
Sakib Noman <skbnmn@gmail.com>
2021-03-14 01:53:44 +0600

    new user login function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import { useState } from "react";
 import './App.css';
 import firebaseConfig from './firebase.config';
 
 
-firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) { firebase.initializeApp(firebaseConfig); }
 
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: '',
+    password: ''
+  })
 
   const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -17,9 +26,59 @@ function App() {
       })
   }
 
+  const handleBlur = e => {
+    let isFieldValid = true;
+    if (e.target.name === 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+
+    }
+    if (e.target.name === 'password') {
+      const isPasswordValid = e.target.value.length >= 6;
+      const isPasswordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && isPasswordHasNumber;
+    }
+
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+
+  }
+
+  const handleSubmit = (e) => {
+    if (user.email && user.password) {
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then((userCredential) => {
+          // Signed in 
+          var user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+
+        });
+    }
+    e.preventDefault();
+  }
   return (
     <div className="App">
       <button onClick={handleSignIn}  >Sign In</button>
+
+      {/* <h1>Our Own Authentication</h1>
+      <h3>Name: {user.name}</h3>
+      <h4>Email: {user.email}</h4>
+      <h4>Password: {user.password}</h4> */}
+      <form onSubmit={handleSubmit}>
+        <input onBlur={handleBlur} type="text" name="name" placeholder="Your name" /> <br />
+        <input onBlur={handleBlur} type="text" name="email" placeholder="Your email" />
+        <br />
+        <input onBlur={handleBlur} type="password" name="password" placeholder="Your password" />
+        <br />
+        <input type="submit" value="SUBMIT" />
+      </form>
     </div>
   );
 }
